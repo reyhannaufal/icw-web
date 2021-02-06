60d4ac5e2dbf017582632d0dd2a21bed9ba45966
Reyhan <reyhannaufal21@gmail.com>
2021-02-07 00:16:33 +0700

    feat: dahboard index

---
+++ resources/views/dashboard/user/dashboard.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <div class="py-40">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <x-jet-welcome class="mt-16" />
+        </div>
+    </div>
+</x-app-layout>
