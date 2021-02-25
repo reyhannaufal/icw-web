9334c7af53009208a82c0e9cbe306ebb43f106b8
William <wiliamwijaya1985@gmail.com>
2021-02-26 00:59:22 +0700

    Change button padding

--- resources/views/dashboard/admin/panel.blade.php
+++ resources/views/dashboard/admin/panel.blade.php
@@ -41,17 +41,20 @@
     </div>
 
     {{-- table --}}
-    <div class="pb-10">
+    <div class="md:pb-10 sm:pb-5">
         {{-- Section Heading --}}
-        <div class="flex items-center justify-between border-b border-gray-200 align-center mx-2 py-2 my-10 max-w-7xl mx-auto sm:mx-6 lg:mx-8">
-            <div>
+        <div class="px-2 py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="pb-5 border-b border-gray-200 sm:flex sm:items-center sm:justify-between">
                 <h3 class="text-lg leading-6 font-medium text-gray-900">
                     <strong>Daftar Peserta</strong>
                 </h3>
+                <div class="mt-3 sm:mt-0 sm:ml-4">
+                    <button type="button"
+                            class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Cetak Peserta
+                    </button>
+                </div>
             </div>
-            <button class="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded">
-                Cetak Peserta
-            </button>
         </div>
 
         <div class="bg-gray-100 text-gray-900 tracking-wider leading-normal">
