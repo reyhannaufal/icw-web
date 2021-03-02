0f574a49c3242e832ab12cd97a7e231dbbd50b13
William <wiliamwijaya1985@gmail.com>
2021-03-02 20:08:10 +0700

    Final improvements before deploying to server

--- resources/views/dashboard/home/_quick-links.blade.php
+++ resources/views/dashboard/home/_quick-links.blade.php
@@ -1,11 +1,17 @@
 <section aria-labelledby="quick-links">
-    <div class="rounded-lg overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px">
+    <div class="rounded-lg bg-gray-200 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px">
         <h2 class="sr-only" id="quick-links-title">Quick links</h2>
 
         @foreach ($events as $event)
-            <div class="rounded-tl-lg relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-cyan-500">
-                <x-event.quick-link-body :event="$event"/>
-            </div>
+            @if ($loop->odd && $loop->last)
+                <div class="text-center rounded-bl-lg  w-full relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset col-start-1 col-end-3 focus-within:ring-cyan-500">
+                    <x-event.quick-link-body :event="$event"/>
+                </div>
+            @else
+                <div class="text-justify rounded-tl-lg relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-cyan-500">
+                    <x-event.quick-link-body :event="$event"/>
+                </div>
+            @endif
         @endforeach
     </div>
 </section>
