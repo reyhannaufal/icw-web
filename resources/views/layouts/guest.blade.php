096359120d2b42aa4bb376150af91d8d36fe7242
c_wasfun <59334824+reyhannaufal@users.noreply.github.com>
2021-03-02 18:31:53 +0700

    update: frontend (#45)
    * fix faker
    
    * fix faker
    
    * fix faker
    
    * fix faker
    
    * update: frontend
    
    * update seeder and add eventuserdashbaord
    
    * update: frontend
    
    * update: frontend update
    
    * add become-partner feature
    
    * add become-partner feature
    
    * frontend update

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,8 +4,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="@yield('description')" />
+    <meta name="description"  content="@yield('description')" />
     <title>ICW | @yield('title')</title>
+
+    {{-- seo--}}
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta property="og:title" content="Indonesian Corrosion Week -  @yield('title')" />
+    <meta property="og:description" content=" Indonesian Corrossion Week (ICW) 2021 merupakan sebuah event besar, yang diselenggarakan oleh
+                    Indonesian Association Corrossion Student Chapter, Institut Teknologi Sepuluh Nopember." />
+    <meta property="og:image" content="https://res.cloudinary.com/dogxsl2bo/image/upload/v1614678168/ms-icon-310x310_du9pa0.png" />
+
+    <link rel="shortcut icon" href="{{\Illuminate\Support\Facades\URL::to('favicon.ico')}}" type="image/x-icon">
+    <link rel="apple-touch-icon" href="{{\Illuminate\Support\Facades\URL::to('favicon.ico')}}">
+
+
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap">
 
