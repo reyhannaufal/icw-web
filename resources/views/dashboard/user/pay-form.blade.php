0df41d2fb31543dd76d7ecd02e49814c179eb090
c_wasfun <59334824+reyhannaufal@users.noreply.github.com>
2021-03-07 02:47:10 +0700

    update: free event payment dsc (#57)
    * fix faker
    
    * fix faker
    
    * fix faker
    
    * fix faker
    
    * update: frontend
    
    * update seeder and add eventuserdashbaord
    
    * update: frontend
    
    * update: frontend update
    
    * add become-partner feature
    
    * add become-partner feature
    
    * frontend update
    
    * issue: about & footer
    
    * update: adding tiktok icon
    
    * update: free event payment

--- resources/views/dashboard/user/pay-form.blade.php
+++ resources/views/dashboard/user/pay-form.blade.php
@@ -11,10 +11,30 @@
                     <li>Tunggu untuk konfirmasi admin</li>
                 </ul>
             </div>
+            @if ($event->name != 'Paper Competition')
+                <div class="mt-5 mx-auto">
+                    <h3 class="text-xl text-gray-800">Cara pembayaran <br> <span
+                                class="text-indigo-800">Event {{$event->name}}.</span></h3>
+                    <ul class="mt-3 list-decimal text-gray-600">
+                        <li>Upload bukti memfollow instagram @indocoritssc.</li>
+                        <li>Upload bukti memfollow instagram @icw2021.</li>
+                        <li>Upload bukti subscribe youtube Indocor ITS SC <a href="https://bit.ly/YOUTUBEINDOCORITSSC"
+                                                                             class="underline text-blue-600">bit.ly/YOUTUBEINDOCORITSSC</a>.
+                        </li>
+                        <li>Upload bukti share poster event terkait dan tag 3 teman dan tag @icw2021.</li>
+                        <li>Semua file dikumpulkan pada satu google drive.</li>
+                        <li>Link google drive harap di set untuk dapat dibuka secara umum. <br> <a
+                                    href="https://support.google.com/drive/answer/2494822?co=GENIE.Platform%3DDesktop&hl=en"
+                                    class="underline text-blue-600">Bantuan untuk membuka drive secara umum</a>.
+                        </li>
+                        <li>Upload link google drive anda dibawah.</li>
+                    </ul>
+                </div>
+            @endif
         </div>
         <x-jet-authentication-card>
 
-            <x-jet-validation-errors class="mb-4" />
+            <x-jet-validation-errors class="mb-4"/>
 
             @if (isset($payment_info))
                 <div class="mb-6">
@@ -29,64 +49,71 @@
                 <div class="mb-6 text-center text-base font-medium text-gray-900">
                     <strong>
                         @if ($event->isFree())
-                            Gratis
+                            Event ini Gratis
                         @else
                             Rp. {{ $event->price }}
                         @endif
                     </strong>
+                    @if($event->isFree())
+                        <h3 class="mt-3 animate-bounce">Upload link drive kamu dibawah!</h3>
+                    @endif
                 </div>
             @endif
 
             @if (!$event->isFree())
                 <div class="mb-12">
-                <h2 class="text-base font-medium text-gray-900" id="announcements-title">
-                    Pembayaran bisa dilakukan melalui:
-                </h2>
-                <div class="flow-root mt-6">
-                    <ul class="-my-5 divide-y divide-gray-200">
-                        @forelse($bills as $bill)
-                            <li class="py-5">
-                                <div class="relative">
-                                    <h3 class="text-sm font-semibold text-gray-800">
-                                        <!-- Extend touch target to entire panel.blade.php -->
-                                        <span class="absolute inset-0" aria-hidden="true"></span>
-                                        {{ $bill->bank_name }}
-                                    </h3>
-                                    <p class="mt-1 text-sm text-gray-600 line-clamp-2">
-                                        {{ $bill->account_name }}
-                                    </p>
-                                    <p class="mt-1 text-sm text-gray-600 line-clamp-2">
-                                        {{ $bill->account_number }}
-                                    </p>
-                                </div>
-                            </li>
-                        @empty
-                            <p class="py-5 relative text-sm font-semibold text-gray-800">Tidak ada info rekening.</p>
-                        @endforelse
-                    </ul>
+                    <h2 class="text-base font-medium text-gray-900" id="announcements-title">
+                        Pembayaran bisa dilakukan melalui:
+                    </h2>
+                    <div class="flow-root mt-6">
+                        <ul class="-my-5 divide-y divide-gray-200">
+                            @forelse($bills as $bill)
+                                <li class="py-5">
+                                    <div class="relative">
+                                        <h3 class="text-sm font-semibold text-gray-800">
+                                            <!-- Extend touch target to entire panel.blade.php -->
+                                            <span class="absolute inset-0" aria-hidden="true"></span>
+                                            {{ $bill->bank_name }}
+                                        </h3>
+                                        <p class="mt-1 text-sm text-gray-600 line-clamp-2">
+                                            {{ $bill->account_name }}
+                                        </p>
+                                        <p class="mt-1 text-sm text-gray-600 line-clamp-2">
+                                            {{ $bill->account_number }}
+                                        </p>
+                                    </div>
+                                </li>
+                            @empty
+                                <p class="py-5 relative text-sm font-semibold text-gray-800">Tidak ada info
+                                    rekening.</p>
+                            @endforelse
+                        </ul>
+                    </div>
                 </div>
-            </div>
             @endif
 
-            <form method="POST" action="{{ route('event-register', Str::slug($event->name, '-')) }}" enctype="multipart/form-data">
+            <form method="POST" action="{{ route('event-register', Str::slug($event->name, '-')) }}"
+                  enctype="multipart/form-data">
                 @csrf
 
                 @if (!$event->isFree())
                     <div>
-                        <x-jet-label for="payment_receipt" value="{{ __('Bukti bayar') }}" />
-                        <x-jet-input id="payment_receipt" class="block mt-1 w-full" type="file" accept="image/*" name="payment_receipt" required autofocus />
+                        <x-jet-label for="payment_receipt" value="{{ __('Bukti bayar') }}"/>
+                        <x-jet-input id="payment_receipt" class="block mt-1 w-full" type="file" accept="image/*"
+                                     name="payment_receipt" required autofocus/>
                     </div>
                 @endif
 
                 @if ($event->name != 'Paper Competition')
                     <div class="{{ ($event->isFree()) ?: 'mt-4' }}">
-                        <x-jet-label for="gdrive" value="{{ __('Link Google Drive') }}" />
+                        <x-jet-label for="gdrive" value="{{ __('Link Google Drive') }}"/>
                         <x-jet-input id="gdrive"
                                      class="h-8 font-sans block mt-1 w-full border border-black border-opacity-100"
                                      type="text" name="gdrive"
                                      required autofocus
                         />
                     </div>
+
                 @endif
 
                 <div class="flex items-center justify-end mt-6">
